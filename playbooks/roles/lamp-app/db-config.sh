18246e308fb84b48e219d00c48cf3b4686334e45
Aleff Morais <aleff.morais@outlook.com>
2024-02-20 19:42:00 -0300

    playbook for lamp app

---
+++ playbooks/roles/lamp-app/db-config.sh
@@ -0,0 +1,6 @@
+# !/bin/bash
+mysql
+CREATE DATABASE ecomdb;
+CREATE USER 'ecomuser'@'localhost' IDENTIFIED BY 'ecompassword';
+GRANT ALL PRIVILEGES ON *.* TO 'ecomuser'@'localhost';
+FLUSH PRIVILEGES;
